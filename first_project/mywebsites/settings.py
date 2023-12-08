4e0dfe22c769b096300bca928ec3604d18103e83
Nanagyamera <nanagyamera3@gmail.com>
2023-12-08 16:20:49 -0600

    Added my Django project code

--- first_project/mywebsites/settings.py
+++ first_project/mywebsites/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+"""import django_heroku
+import dj_database_url """
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,13 +77,23 @@ WSGI_APPLICATION = 'mywebsites.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '43CfbgcFAeEAcf3cDd-F43FfdB21G*CF',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '29776',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -122,7 +132,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-django_heroku.settings(locals())
+"""django_heroku.settings(locals())"""
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
