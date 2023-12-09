52da5b500368c56f415f40f856ccc33896b61cd5
Nanagyamera <nanagyamera3@gmail.com>
2023-12-09 16:19:34 -0600

    Added my Django project code

--- first_project/mywebsites/settings.py
+++ first_project/mywebsites/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ojy59$d!em&-4b3de7o092pln#4ig9t5o%9_n!5__ojy=xxl1='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,9 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'static/'
+"""STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]"""
 """django_heroku.settings(locals())"""
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
