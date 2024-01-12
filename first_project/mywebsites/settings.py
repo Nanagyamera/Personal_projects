83a75e40c5306d6500ff2b125647f7c8dd957a9a
Nanagyamera <nanagyamera3@gmail.com>
2024-01-11 23:45:26 -0600

    removed the corsheaders

--- first_project/mywebsites/settings.py
+++ first_project/mywebsites/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['jams-site1.up.railway.app', '127.0.0.1']
 
